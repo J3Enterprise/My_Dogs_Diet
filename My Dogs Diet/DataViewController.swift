4858868def6595fb36782ece572ace2408715a90
jamesjohnson763 <jamesjohnson763@gmail.com>
2015-12-23 21:46:06 -0600

    Added names

--- My Dogs Diet/DataViewController.swift
+++ My Dogs Diet/DataViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class DataViewController: UIViewController {
 
-    let dogNames = ["Bosco", "Max", "Haha","Pluto"]
+    let dogNames = ["Bosco", "Max", "Haha", "Pluto", "Brutus"]
     
     @IBAction func dogButtonPressed(sender: AnyObject) {
         let randomValue = Int(arc4random_uniform(UInt32(dogNames.count)))
         
+        self.dogName.text = self.dogNames[randomValue]
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    // Detect shake and change the text to random dog name
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        let randomValue = Int(arc4random_uniform(UInt32(dogNames.count)))
         self.dogName.text = self.dogNames[randomValue]
     }
     
